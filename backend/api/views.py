50b1652426d682e157eae02f0b34416cf43d6ab0
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-05-31 12:27:20 +0500

    add: learned about passing body and params in a request from python and checking in views

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,15 @@
 from django.http import JsonResponse
-
+import json 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({"message":"Hy There this is your django api response"})
+    # request body
+    body = request.body # byte string of json response
+    params = dict(request.GET)
+    print(params)
+    print(request.headers)
+    data = {}
+    try:
+        data = json.loads(body)
+    except:
+        pass
+    print(data)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -3,5 +3,5 @@ from requests import get
 # endpoint = "https://www.github.com"
 endpoint = "http://localhost:8000/api"
 
-get_response = get(endpoint)
+get_response = get(endpoint, params={"abc":12}, json={"query":"hello"})
 print(get_response.json())
