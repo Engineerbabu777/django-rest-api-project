b9830c460b4d503335ecac361404ab66c3ae70fc
Engineerbabu777 <awaismumtaz0099@gmail.com>
2024-06-01 09:12:21 +0500

    add: rest framework serializers

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,11 @@ from django.http import JsonResponse,HttpResponse
 import json 
 from products.models import Product
 from django.forms.models import model_to_dict
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products.serializers import ProductSerializer
 
+@api_view(["GET","POST"])
 def api_home(request, *args, **kwargs):
     # request body
     # body = request.body # byte string of json response
@@ -17,11 +21,14 @@ def api_home(request, *args, **kwargs):
     #     pass
     # print(data)
     # return JsonResponse(data)
-    model_data = Product.objects.all().order_by("?").first()
+    
+   #  if request.method != "POST":
+      #  return Response({"message":"Status not found!"},status=400)
+    instance = Product.objects.all().order_by("?").last()
     data = {}
-    if model_data:
-       data = model_to_dict(model_data, fields=['id', 'title', 'price'])
-    return JsonResponse(data)
+    if instance:
+       data = model_to_dict(instance, fields=['id', 'title', 'price',"sale_price","my_discount"])
+    return Response(ProductSerializer(instance).data)
     #    data = dict(data)
     #    json_data_str = json.dumps(data)
     # return HttpResponse(json_data_str, headers={"content-type":"application/json"})
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,7 @@
 from requests import get
 
 # endpoint = "https://www.github.com"
-endpoint = "http://localhost:8000/api"
+endpoint = "http://localhost:8080/api"
 
 get_response = get(endpoint, params={"product_id":1}, json={"query":"hello"})
 print(get_response.json())
